6740ea1c6949b7404ca0fdd7f1b89bbfc44d3dce
Duality <332583738+Duality-TOP@users.noreply.github.com>
2026-09-26 13:22:11 -0300

    solved using variables which has a expression inside them. and compressing them into another variable + returning its value :D

--- foundations/loops_and_arrays/05_leapYears/leapYears.js
+++ foundations/loops_and_arrays/05_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isDivisibleByFour = year % 4 === 0;
+    const isDivisibleByOneHundred = year % 100 === 0;
+    const isDivisibleByFourHundred = year % 400 === 0;
 
+    const isALeapYear = isDivisibleByFour && !isDivisibleByOneHundred || isDivisibleByFourHundred;
+    return isALeapYear;
 };
 
 // Do not edit below this line
