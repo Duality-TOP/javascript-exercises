39582f81ce9ee898b2dea0e0e98c44b5dad5c881
Duality <332583738+Duality-TOP@users.noreply.github.com>
2026-09-26 13:01:46 -0300

    solved using the array destructuring (not sure if i spelt it right, but ok)

--- foundations/loops_and_arrays/04_sumAll/sumAll.js
+++ foundations/loops_and_arrays/04_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
 
+    if (min > max) { 
+        [min, max] = [max, min]; // this swaps their positions if min is greater than max, (called 'destructuring')
+        //  so we always count from the smaller number to the larger number
+     } 
+
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
